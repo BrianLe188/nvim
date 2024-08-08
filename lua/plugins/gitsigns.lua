2b8f0090f463f00cff2902cac5a28d77d1162ced
brian.h <anhkun123456@gmail.com>
2024-08-08 16:48:16 +0700

    init

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,7 @@
+return {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+        current_line_blame = true,
+        current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+    }
+}
