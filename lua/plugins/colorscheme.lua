6d093bfeda1196f9efba9de479e7d7b2e265edae
Việt Anh Lê <anhkun123456@gmail.com>
2025-10-11 12:01:57 +0700

    mise

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -69,11 +69,6 @@ return {
 		{
 			"akinsho/bufferline.nvim",
 			optional = true,
-			opts = function(_, opts)
-				if (vim.g.colors_name or ""):find("catppuccin") then
-					opts.highlights = require("catppuccin.groups.integrations.bufferline").get_theme()
-				end
-			end,
 		},
 	},
 }
